70c53486737f0215801b307c197f0ce7b4469ac9
AlexTzortzakis <p22171@unipi.gr>
2026-02-27 22:42:47 +0200

    coin script done

---
+++ Assets/My Scripts/CoinScript.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CoinScript : MonoBehaviour
+{
+    private bool isPlayerInside;
+    private void Start()
+    {
+        
+    }
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter called with: " + other.name);
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInside = true;
+            Debug.Log("Player entered coin collider");
+        }
+    }
+
+     private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInside = false;
+            Debug.Log("Player exited coin collider");
+        }
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.fKey.wasPressedThisFrame)
+        {
+            Debug.Log("F pressed. isPlayerInside: " + isPlayerInside);
+            if (isPlayerInside)
+            {
+                Disappear();
+            }
+        }
+    }
+
+    
+    private void Disappear()
+    {
+        gameObject.SetActive(false);
+    }
+}
